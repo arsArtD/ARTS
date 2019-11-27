4b791f67f43ea7db2c2d8a1e791c67ef51a47ffb
arsArtD <xueshuxuede@yeah.net>
2019-11-27 08:51:40 +0800

    week15 算法补充

--- algorithm/arts_week15_20191119/tree.js
+++ algorithm/arts_week15_20191119/tree.js
@@ -13,20 +13,44 @@ function buildTree(arr) {
     }
     if(arr.length == 0)  return null;
     if(arr.length == 1)  return new TreeNode(arr[0]);
-    var root = new TreeNode(arr[0]);
-    for(var i=1; i<arr.length; i++) {
-        if(2*i+1 < arr.length) {
-            root.left = new TreeNode(arr[2*i+1]);
-        }
-        if(2*i+2 < arr.length) {
-            root.right = new TreeNode(arr[2*i+2]);
-        }
-        if(root.left != null && root.right != null) {
-            root = root.left;
-        } else {
 
+    var depth = Math.ceil(Math.log2(arr.length));
+    var root = null;
+    var data = [];//存储各个子树的数据
+
+    // 按照层级遍历
+    for(var d = 1; d <= depth; d++) {
+        if(d == 1) {
+            data[0] =  new TreeNode(arr[0]);
+        } else {
+            var currDepthLastIndex = 0;
+            var currDepthFirstIndex = Math.pow(2,d-1) - 1;
+            if (d != depth) {
+                currDepthLastIndex = Math.pow(2,d) - 2;
+            } else {
+                currDepthLastIndex = arr.length - 1;
+            }
+            for(var index = currDepthFirstIndex; index <= currDepthLastIndex; index++) {
+                data[index] =  new TreeNode(arr[index]);
+
+                var temp;
+                if((index % 2) != 0) {
+                    temp = data[Math.floor((index-1)/2)];
+                    //奇数为左节点
+                    temp && (temp['left'] = data[index]);
+                } else {
+                    temp = data[Math.floor((index-2)/2)];
+                    //偶数为右节点
+                    temp && (temp['right'] = data[index]);
+                }
+                //console.log(arr[index], '=====', index, d);
+            }
         }
     }
+    //console.log(data);
+    return data[0];
 }
 
-buildTree([3,5,1,6,2,0,8,null,null,7,4]);
+var tree = buildTree([3,5,1,6,2,0,8,null,null,7,4]);
+console.log(tree)
+console.log(JSON.stringify(tree))
